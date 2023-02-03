4030b82aa10d8df5354168d14e94afaa33c1a965
Grace Joh <graceehjoh@gmail.com>
2023-02-02 23:01:51 -0600

    test: add test for node

---
+++ spec/node_spec.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require './lib/node'
+
+RSpec.describe Node do
+  
+  describe '#initialize' do
+
+    it 'exists' do
+      node = Node.new("first")
+
+      expect(node).to be_an_instance_of(Node)
+    end
+
+    it 'has a sound' do
+      node = Node.new("first")
+
+      expect(node.sound).to eq("first")
+    end
+
+    it 'has a next_node' do
+      node = Node.new("first")
+
+      expect(node.next_node).to eq(nil)
+    end
+  end
+end
