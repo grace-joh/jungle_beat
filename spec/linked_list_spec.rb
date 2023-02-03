ba5dcc92421709ab74e731c138445bd81bd115b0
Grace Joh <graceehjoh@gmail.com>
2023-02-03 10:21:33 -0600

    test: add test for append first node

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,4 +1,5 @@
 require './lib/linked_list'
+require './lib/node'
 
 RSpec.describe LinkedList do
 
@@ -17,4 +18,16 @@ RSpec.describe LinkedList do
     end
   end
 
+  describe '#append' do
+
+    it 'appends the first sound as the head node' do
+      list = LinkedList.new
+      list.append("doop")
+
+      expect(list.head).to be_a(Node)
+      expect(list.head.data).to eq("doop")
+      expect(list.head.next_node).to eq(nil)
+      expect(list.count).to eq(1)
+    end
+  end
 end
