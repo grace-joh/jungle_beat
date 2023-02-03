450369f6c60f95e95f44f4c20146ef902da5ce3e
Grace Joh <graceehjoh@gmail.com>
2023-02-03 14:42:34 -0600

    fix: add correct return value to append

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,15 +1,21 @@
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :count, :current_node
 
   def initialize
     @head = nil
     @count = 0
+    @current_node = nil
   end
 
   def append(sound)
-    if @head == nil
+    if count >= 1
+      @current_node.next_node = Node.new(sound)
+      @current_node = @current_node.next_node
+    elsif count == 0
       @head = Node.new(sound)
-      @count += 1
+      @current_node = @head
     end
+    @count += 1
+    sound
   end
 end
