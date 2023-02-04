57b02f0e4603c8ba9823a2ff149e306626356516
Grace Joh <graceehjoh@gmail.com>
2023-02-03 22:41:59 -0600

    test: add test for to_string

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -22,10 +22,10 @@ RSpec.describe LinkedList do
 
     it 'appends the first sound as the head node' do
       list = LinkedList.new
-      list.append("doop")
+      list.append('doop')
 
       expect(list.head).to be_a(Node)
-      expect(list.head.data).to eq("doop")
+      expect(list.head.data).to eq('doop')
       expect(list.head.next_node).to eq(nil)
       expect(list.count).to eq(1)
       expect(list.current_node).to eq(list.head)
@@ -33,28 +33,46 @@ RSpec.describe LinkedList do
 
     it 'appends a second node as the next node' do
       list = LinkedList.new
-      list.append("doop")
-      list.append("deep")
+      list.append('doop')
+      list.append('deep')
       
 
       expect(list.head).to be_a(Node)
-      expect(list.head.data).to eq("doop")
+      expect(list.head.data).to eq('doop')
       expect(list.head.next_node).to be_a(Node)
-      expect(list.head.next_node.data).to eq("deep")
+      expect(list.head.next_node.data).to eq('deep')
       expect(list.count).to eq(2)
     end
 
     it 'appends a multiple nodes' do
       list = LinkedList.new
-      list.append("doop")
-      list.append("deep")
-      list.append("bop")
-      
+      list.append('doop')
+      list.append('deep')
+      list.append('bop')
+
       expect(list.head).to be_a(Node)
-      expect(list.head.data).to eq("doop")
-      expect(list.current_node.data).to eq("bop")
+      expect(list.head.data).to eq('doop')
+      expect(list.current_node.data).to eq('bop')
       expect(list.count).to eq(3)
     end
   end
 
+  describe '#to_string' do
+
+    it 'make a string of list nodes data' do
+      list = LinkedList.new
+      list.append('doop')
+      list.append('doop')
+      list.append('beep')
+      list.append('bop')
+      list.append('doop')
+      list.append('doo')
+      list.append('dah')
+      list.append('beep')
+      list.append('bop')
+
+
+      expect(list.to_string).to eq('doop doop beep bop doop doo dah beep bop')
+    end
+  end
 end
