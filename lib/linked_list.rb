f75198407c90db2d11bcbe9af418213fde5d3b11
Grace Joh <graceehjoh@gmail.com>
2023-02-03 22:42:35 -0600

    feat: add to_string method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -8,7 +8,7 @@ class LinkedList
   end
 
   def append(sound)
-    if count >= 1
+    if count > 0
       @current_node.next_node = Node.new(sound)
       @current_node = @current_node.next_node
     elsif count == 0
@@ -17,5 +17,20 @@ class LinkedList
     end
     @count += 1
     sound
+  end
+
+
+  def to_string
+    sounds = []
+    if @head != nil
+      sounds << @head.data
+      current_sound_node = @head.next_node
+      until current_sound_node == nil
+        sounds << current_sound_node.data
+        current_sound_node = current_sound_node.next_node
+      end
+    # else (list is empty and head is nil) output some error message?
+    end
+    sounds.join(' ')
   end
 end
