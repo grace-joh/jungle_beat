707817dba1b286713c2dd9e552d87e7f69a4795a
Grace Joh <graceehjoh@gmail.com>
2023-02-05 17:01:21 -0600

    feat: add #append for LinkedList

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -2,8 +2,13 @@ require './lib/linked_list'
 require './lib/node'
 
 class JungleBeat
+  attr_reader :list
 
   def initialize
     @list = LinkedList.new
+  end
+
+  def append(sounds)
+    sounds.split(' ').each {|sound| @list.append(sound)}
   end
 end
