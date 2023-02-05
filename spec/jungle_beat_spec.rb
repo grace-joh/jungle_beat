0950ebb286cfcba7ef7a9e60abf9fecbd5535972
Grace Joh <graceehjoh@gmail.com>
2023-02-05 16:55:45 -0600

    test: update test for LinkedList #initialize and add tests for #append

--- spec/jungle_beat_spec.rb
+++ spec/jungle_beat_spec.rb
@@ -10,6 +10,20 @@ RSpec.describe JungleBeat do
       jb = JungleBeat.new
 
       expect(jb).to be_an_instance_of(JungleBeat)
+      expect(jb.list).to be_an_instance_of(LinkedList)
+      expect(jb.list.head).to be_nil
+    end
+  end
+
+  describe '#append' do
+    
+    it 'makes nodes for each sound in a given string' do
+      jb = JungleBeat.new
+      jb.append("deep doo ditt")
+
+      expect(jb.list.head.data).to eq("deep")
+      expect(jb.list.head.next_node.data).to eq("doo")
+      expect(jb.list.head.next_node.next_node.data).to eq("ditt")
     end
   end
 end
