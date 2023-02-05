5865ca236964f4d91b17dadebc9f5f3d5cf186f5
Grace Joh <graceehjoh@gmail.com>
2023-02-04 22:59:24 -0600

    feat: add #insert to LinkedList

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -34,13 +34,47 @@ class LinkedList
   end
 
   def prepend(sound)
-    if count > 0
+    if @count > 0
       prepend_node = Node.new(sound)
       prepend_node.next_node = @head
       @head = prepend_node
-    elsif count == 0
+    elsif @count == 0
       append(sound)
     end
+    @count += 1
+    sound
+  end
+
+  def insert(insert_num, sound)
+    if @count == 0
+      append(sound)
+    elsif @count > 0
+      if insert_num == 0
+        prepend(sound)
+      elsif insert_num == @count - 1
+        append(sound)
+      elsif insert_num >= @count
+        append(sound)
+        puts "The insert position number is greater than the length of this list."
+        puts "Your new sound has been added to the end of the list."
+      else
+        insert_node = Node.new(sound)
+        # traverse through list and set insert's next node to given position's node
+        post_node = @head
+        insert_num.times do
+          post_node = post_node.next_node
+        end
+        insert_node.next_node = post_node
+
+        # traverse through list to previous node and set it's next node to insert node
+        pre_node = @head
+        (insert_num - 1).times do
+          pre_node = pre_node.next_node
+        end
+        pre_node.next_node = insert_node
+      end
+    end
+    @count += 1
     sound
   end
 end
